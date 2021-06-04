7d11f9956d301f18342f3bb10714bdb681b0cc30
NineR-9R <76840476+NineR-9R@users.noreply.github.com>
2021-06-04 21:54:18 +0300

    Update alphastatus.lua

--- alphastatus.lua
+++ alphastatus.lua
@@ -1,7 +1,7 @@
 if game.Players.LocalPlayer.PlayerGui.GUI.Spectate.FPSpectating then
     local v4l = Instance.new("StringValue",game.Players.LocalPlayer.PlayerGui.GUI.Spectate)
     v4l.Name = "FPSonSpectate"
-    v4l.Value = "60" --FPS
+    v4l.Value = "59" --FPS
 else
     print("Something Wrong..")
 end
