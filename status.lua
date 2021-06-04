7142b4c5a93c0a5b0605059c06031c094f162e7a
NineR-9R <76840476+NineR-9R@users.noreply.github.com>
2021-06-04 18:44:03 +0300

    Update status.lua

--- status.lua
+++ status.lua
@@ -1,3 +1,5 @@
-local v4l = Instance.new("StringValue", game.ReplicatedStorage)
-v4l.Name = "FPSonSpectate"
-v4l.Value = "~39"
+if not game.ReplicatedStorage.FPSonSpectate then
+    local v4l = Instance.new("StringValue", game.ReplicatedStorage)
+    v4l.Name = "FPSonSpectate"
+    v4l.Value = "~39"
+end
