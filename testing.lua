45a5cdfef1db1f1dc7ea524ec3e298671a32e2a4
NineR-9R <76840476+NineR-9R@users.noreply.github.com>
2021-06-04 19:46:29 +0300

    Delete testing.lua

--- testing.lua
+++
@@ -1,7 +0,0 @@
-if game.ReplicatedStorage.FPSonSpectate then
-    --.
-else
-    local v4l = Instance.new("StringValue", game.ReplicatedStorage)
-    v4l.Name = "FPSonSpectate"
-    v4l.Value = "39"
-end
