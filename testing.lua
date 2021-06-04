0b213e43ba472686c0ffbe8473341a71181f9089
NineR-9R <76840476+NineR-9R@users.noreply.github.com>
2021-06-04 18:48:02 +0300

    Create testing.lua

---
+++ testing.lua
@@ -0,0 +1,7 @@
+if game.ReplicatedStorage.FPSonSpectate then
+    --.
+else
+    local v4l = Instance.new("StringValue", game.ReplicatedStorage)
+    v4l.Name = "FPSonSpectate"
+    v4l.Value = "39"
+end
