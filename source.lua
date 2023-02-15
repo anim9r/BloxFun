12fbcc219174fa4f16be18e7d0dbebacf4880429
Anim <76840476+anim9r@users.noreply.github.com>
2023-02-15 15:21:30 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,3 +1,1 @@
---Source code is replaced to other path. Visit https://github.com/anim9r/BloxFun/blob/main/code.lua
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/anim9r/BloxFun/main/code.lua"))()
